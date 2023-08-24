05ff8a3b611d7b1ff1f27116bec3a7fb3dfe01d3
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-08-25 00:36:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
 volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
+
 let allKeys = [],
 audio = new Audio(`tunes/a.wav`); 
 
